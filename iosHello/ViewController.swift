03129fd1188eb57a90c9c5c2a18b16716672c53d
Brian Hanafee <bhanafee@gmail.com>
2017-10-05 09:21:39 -0700

    Basic hello button app

--- iosHello/ViewController.swift
+++ iosHello/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "ioS Hello", message: "Hello yourself", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
